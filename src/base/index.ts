1f509e0beb697ac6d55744e03e0e1b2a7bae229a
ViniciosLugli <vinicioslugli@gmail.com>
2023-12-24 11:56:09 -0300

    fix(index): :fire: Remove old file on base

--- src/base/index.ts
+++
@@ -1,6 +0,0 @@
-import WebServer from '@base/webserver';
-import Route from '@base/route';
-import Endpoint from '@base/endpoint';
-import Middleware from '@base/middleware';
-
-export { WebServer, Route, Endpoint, Middleware };
